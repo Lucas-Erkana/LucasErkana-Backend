80c0034c819281d37730c008e452ce7a28fb597d
Lucas David Erkana <lderkana21x@gmail.com>
2023-09-02 14:19:06 +0200

    Complete

--- server.js
+++ server.js
@@ -4,9 +4,14 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
-mongoose.connect('process.env.DATABASE_URL',{ useNewUrlParser: true})
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 const db = mongoose.connection
-db.on('error', (error)=> console.error(error))
-db.once('open', ()=> console.log('Connected to Database'))
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
 
-app.listen(3000, ()=> console.log('Server Started')) 
+app.use(express.json())
+
+const subscribersRouter = require('./routes/subscribers')
+app.use('/subscribers', subscribersRouter)
+
+app.listen(3000, () => console.log('Server Started'))
